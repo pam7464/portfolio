d3bcf34574711d7d1ce6f0b2fa2c616f789ca102
pam7464 <74638378+pam7464@users.noreply.github.com>
2022-02-17 15:45:01 +0900

    Add files via upload

--- js/my-object.js
+++ js/my-object.js
@@ -1,43 +1,17 @@
- let myObj ={
-     lock:function(target){
-        target.classList.add('lock');
-     },
-     unlock:function(target){
-         target.classList.remove('lock');
-     }
- }
- 
- // NAV OBJECT
- let navObj = {
-    showNav:function(target){
-        target.classList.add('lock');
+let myObj ={
+    lock:function(target){
+    target.classList.add('lock');
     },
-    closeNav:function(target){
-        target.classList.remove('lock');
-    },
-    lockWrap:function(target){
-        target.classList.add('lock');
-    },
-    unLockWrap:function(target){
+    unlock:function(target){
         target.classList.remove('lock');
     },
     setNav:function(navList,wrap,navContainer){
         for(let idx=0;idx<navList.length;idx++){
             navList[idx].addEventListener('click',function(){
-                navObj.unLockWrap(wrap);
-                navObj.closeNav(navContainer);
+                myObj.unlock(wrap);
+                myObj.unlock(navContainer);
             })
         }
     }
 }
-
-// HEADER OBJECT
-let headerObj = {            
-    lockHeader:function(target){
-        target.classList.add('lock');
-    },
-    unLockHeader:function(target){
-        target.classList.remove('lock');
-    },
-}
-
+ 
